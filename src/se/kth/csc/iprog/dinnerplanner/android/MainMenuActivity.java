abd8e83ecd9bbe8c0e14ffbe84ce971d8e5cd5ee
Mattias <Mattias.Folke@gmail.com>
2014-02-06 20:09:57 +0100

    mainmenu is now working

--- src/se/kth/csc/iprog/dinnerplanner/android/MainMenuActivity.java
+++ src/se/kth/csc/iprog/dinnerplanner/android/MainMenuActivity.java
@@ -2,25 +2,42 @@ package se.kth.csc.iprog.dinnerplanner.android;
 
 import se.kth.csc.iprog.dinnerplanner.android.view.MainMenuView;
 import android.app.Activity;
+import android.graphics.Path.FillType;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 
 public class MainMenuActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        // Default call to load previous state
-    	super.onCreate(savedInstanceState);
-    	
-    	// Set the view for the main activity screen
-    	// it must come before any call to findViewById method
-        setContentView(R.layout.choose_menu_top_view);
-        setContentView(R.layout.choose_menu_main_screen);
-        setContentView(R.layout.choose_menu_choose_dish);
-        
-    	// Creating the view class instance
-    	MainMenuView mainView = new MainMenuView(findViewById(R.id.this_is_chooseMenu_mainScreen));
-    	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// Default call to load previous state
+		super.onCreate(savedInstanceState);
 
-    }
+		LayoutParams v = new ViewGroup.LayoutParams(
+				ViewGroup.LayoutParams.MATCH_PARENT,
+				ViewGroup.LayoutParams.MATCH_PARENT);
+		/*
+		 * LayoutInflater inflater = getLayoutInflater();
+		 * getWindow().addContentView(inflater.inflate(R.layout.extra_layout,
+		 * null), new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
+		 * ViewGroup.LayoutParams.FILL_PARENT));
+		 */
 
+		LayoutInflater inflater = getLayoutInflater();
+		getWindow().addContentView(
+				inflater.inflate(R.layout.choose_menu_main_screen, null),
+				new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+						ViewGroup.LayoutParams.MATCH_PARENT));
+		getWindow().addContentView(
+				inflater.inflate(R.layout.choose_menu_top_view, null),
+				new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+						ViewGroup.LayoutParams.MATCH_PARENT));
+
+		// Creating the view class instance
+		// MainMenuView mainView = new
+		// MainMenuView(findViewById(R.id.this_is_chooseMenu_mainScreen));
+
+	}
 }
