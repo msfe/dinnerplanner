f940d851b8bc98608b418f48ae76c609af3fa187
Mattias <Mattias.Folke@gmail.com>
2014-02-06 17:59:00 +0100

    renamed exampleView to startView

--- src/se/kth/csc/iprog/dinnerplanner/android/view/StartView.java
+++ src/se/kth/csc/iprog/dinnerplanner/android/view/StartView.java
@@ -1,22 +1,22 @@
 package se.kth.csc.iprog.dinnerplanner.android.view;
 
 import se.kth.csc.iprog.dinnerplanner.android.R;
+import android.R.string;
 import android.view.View;
 import android.widget.TextView;
 
-public class ExampleView {
+public class StartView {
 
 	View view;
 
-	public ExampleView(View view) {
+	public StartView(View view) {
 
 		// store in the class the reference to the Android View
 		this.view = view;
 
-		TextView example = (TextView) view.findViewById(R.id.blabla);
+		TextView example = (TextView) view.findViewById(R.id.startInfo888);
 		
-		
-		//example.setText("Hello world");
+		example.setText(R.string.welcomeString);
 
 		// Setup the rest of the view layout
 	}
