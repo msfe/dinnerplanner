a8cc1420ae389f2a0218925a2d116343585d2637
Martin Andersen <martin.erik.andersen91@gmail.com>
2014-02-07 15:06:49 +0100

    added the popup-frame for the coose_dish-screen

--- src/se/kth/csc/iprog/dinnerplanner/android/MainMenuActivity.java
+++ src/se/kth/csc/iprog/dinnerplanner/android/MainMenuActivity.java
@@ -1,8 +1,10 @@
 package se.kth.csc.iprog.dinnerplanner.android;
 
 import se.kth.csc.iprog.dinnerplanner.android.view.MainMenuView;
+import se.kth.csc.iprog.dinnerplanner.android.view.PopUpMenuView;
 import se.kth.csc.iprog.dinnerplanner.android.view.TopMenuView;
 import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
+import se.kth.csc.iprog.dinnerplanner.model.Dish;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -27,12 +29,18 @@ public class MainMenuActivity extends Activity {
 				inflater.inflate(R.layout.choose_menu_top_view, null),
 				new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
 						ViewGroup.LayoutParams.MATCH_PARENT));
+		getWindow().addContentView(
+                inflater.inflate(R.layout.choose_menu_choose_dish, null),
+                new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT));
 
 		// Creating the view class instance
 		MainMenuView mainView = new MainMenuView(
 				findViewById(R.id.this_is_chooseMenu_mainScreen), model);
 		TopMenuView topMenu = new TopMenuView(
 				findViewById(R.id.this_is_chooseMenu_topView), model);
+		PopUpMenuView popUpMenu = new PopUpMenuView(
+                findViewById(R.id.this_is_chooseMenu_chooseDish), (Dish) model.getDishes().toArray()[0], model);
 
 	}
 }
