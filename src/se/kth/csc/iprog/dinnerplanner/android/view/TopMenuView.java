5c068bf52b9b9bd00a74736f72886b3f09f2e01c
Mattias <Mattias.Folke@gmail.com>
2014-02-10 16:45:37 +0100

    update so that spinner now takes value from model when its loaded

--- src/se/kth/csc/iprog/dinnerplanner/android/view/TopMenuView.java
+++ src/se/kth/csc/iprog/dinnerplanner/android/view/TopMenuView.java
@@ -19,14 +19,15 @@ public class TopMenuView {
 		// store in the class the reference to the Android View
 		this.view = view;
 		this.model = model;
-		
+
 		TextView cost = (TextView) view.findViewById(R.id.dynamicCost);
 		cost.setText(String.valueOf(model.getTotalMenuPrice()) + " kr");
-		
-		Spinner participants = (Spinner) view.findViewById(R.id.numGuestsDropDown);
-		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(view.getContext(), R.array.participants_array, android.R.layout.simple_spinner_dropdown_item);
-		int guests = adapter.getPosition("2");
-		participants.setSelection(guests);
+
+		Spinner participants = (Spinner) view
+				.findViewById(R.id.numGuestsDropDown);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+				view.getContext(), R.array.participants_array,
+				android.R.layout.simple_spinner_dropdown_item);
 		// Specify the layout to use when the list of choices appears
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		// Apply the adapter to the spinner
@@ -36,17 +37,17 @@ public class TopMenuView {
 			@Override
 			public void onItemSelected(AdapterView<?> parent, View view,
 					int pos, long id) {
-				CharSequence updated = (CharSequence) parent.getItemAtPosition(pos);
+				CharSequence updated = (CharSequence) parent
+						.getItemAtPosition(pos);
 				model.setNumberOfGuests(Integer.parseInt(updated.toString()));
 			}
 
 			@Override
 			public void onNothingSelected(AdapterView<?> arg0) {
 			}
-			
+
 		});
-		
-		
+		participants.setSelection(adapter.getPosition(String.valueOf(model.getNumberOfGuests())));
 
 		// Setup the rest of the view layout
 	}
