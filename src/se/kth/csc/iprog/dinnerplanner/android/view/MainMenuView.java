bda17c318243365efc25674757ff03bc5202ad81
Robin Chowdhury <robinch88@gmail.com>
2014-02-18 15:54:01 +0100

    Did some refractoring, added a TextUnderImageView Class

--- src/se/kth/csc/iprog/dinnerplanner/android/view/MainMenuView.java
+++ src/se/kth/csc/iprog/dinnerplanner/android/view/MainMenuView.java
@@ -2,22 +2,13 @@ package se.kth.csc.iprog.dinnerplanner.android.view;
 
 import java.util.Set;
 
-import se.kth.csc.iprog.dinnerplanner.android.MainMenuActivity;
 import se.kth.csc.iprog.dinnerplanner.android.R;
-import se.kth.csc.iprog.dinnerplanner.android.classes.DishImageButton;
 import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
 import se.kth.csc.iprog.dinnerplanner.model.Dish;
 import android.annotation.SuppressLint;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.view.ViewGroup.LayoutParams;
-import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.LinearLayout.LayoutParams;
 
 public class MainMenuView {
 
@@ -56,50 +47,19 @@ public class MainMenuView {
 
 	}
 
-	@SuppressLint("NewApi")
 	public void addDishToScrollView(LinearLayout linearLayout, Dish dish) {
-		//Create imagebutton
-		DishImageButton imageButton = new DishImageButton(view.getContext(),
-				dish);
-		int id = view.getResources().getIdentifier(
-				dish.getImage().split("\\.")[0], "drawable",
-				this.view.getContext().getPackageName());
-		imageButton.setImageResource(id);
-		int a = View.generateViewId();
-		imageButton.setId(a);
-		
-		//Create textview
-		TextView textView = new TextView(view.getContext());
-		textView.setText(dish.getName());
-		
-		//Create a relative layout
-		RelativeLayout rel = new RelativeLayout(view.getContext());
-		rel.addView(imageButton);
-		rel.addView(textView);
-		
-		LinearLayout.LayoutParams relLay = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-		relLay.rightMargin = convertDpToPx(10);
-		relLay.leftMargin = convertDpToPx(10);
-		rel.setLayoutParams(relLay);
-		
-		//Align text under imagebutton
-		RelativeLayout.LayoutParams relParams =  new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-		relParams.addRule(RelativeLayout.BELOW, a);
-		textView.setLayoutParams(relParams);
-		rel.setGravity(Gravity.CENTER);
-		
-		
-		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-				convertDpToPx(100), convertDpToPx(100));
-		imageButton.setLayoutParams(params);
-		
-		//Add the image_under_button-view to the linearlayout
-		linearLayout.addView(rel);
-	}
 
-	private int convertDpToPx(int dp) {
-		float scale = view.getContext().getResources().getDisplayMetrics().density;
-		return (int) (dp * scale + 0.5f);
+		TextUnderImageView txtUImageView = new TextUnderImageView(
+				view.getContext());
+		txtUImageView.addDishImageButton(dish, txtUImageView.convertDpToPx(view.getContext(), 100),
+				txtUImageView.convertDpToPx(view.getContext(), 100));
+		txtUImageView.addTextView(dish.getName());
+		LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(
+				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		param.rightMargin = txtUImageView.convertDpToPx(view.getContext(), 10);
+		param.leftMargin = txtUImageView.convertDpToPx(view.getContext(), 10);
+		txtUImageView.setLayoutParams(param);
+		linearLayout.addView(txtUImageView);
 	}
 
 }
--- src/se/kth/csc/iprog/dinnerplanner/android/view/TextUnderImageView.java
+++ src/se/kth/csc/iprog/dinnerplanner/android/view/TextUnderImageView.java
@@ -1,29 +1,70 @@
 package se.kth.csc.iprog.dinnerplanner.android.view;
 
+import se.kth.csc.iprog.dinnerplanner.android.classes.DishImageButton;
+import se.kth.csc.iprog.dinnerplanner.model.Dish;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.Gravity;
 import android.view.View;
-import android.widget.ImageButton;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class TextUnderImageView extends RelativeLayout{
-	private ImageButton imageButton;
+	private DishImageButton imageButton;
 	private TextView textView;
 	
+	private Context context;
+	private int imageId;
+	
+	@SuppressLint("NewApi")
 	public TextUnderImageView(Context context) {
 		super(context);
-		
-		textView = new TextView(context);
-		imageButton = new ImageButton(context);
-	
-		getLayoutParams().width = LayoutParams.WRAP_CONTENT;
-		getLayoutParams().height = LayoutParams.WRAP_CONTENT;
+		this.context = context;
 		setGravity(Gravity.CENTER);
+		imageId = View.generateViewId();
+	}
+	
+	public void addDishImageButton(Dish dish, int imgHeight, int imgWidth){
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+				imgHeight, imgWidth);
 		
+		DishImageButton imageButton = new DishImageButton(context,
+				dish);
+		int imgId = context.getResources().getIdentifier(
+				dish.getImage().split("\\.")[0], "drawable",
+				context.getPackageName());
+		imageButton.setImageResource(imgId);
+		imageButton.setId(imageId);
+		
+		
+		imageButton.setLayoutParams(params);
+		addView(imageButton);
+	}
+	
+	public void addTextView(String text){
+		RelativeLayout.LayoutParams relParams =  new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT,
+				LayoutParams.WRAP_CONTENT);
+		
+		TextView textView = new TextView(context);
+		textView.setText(text);
+		
+		relParams.addRule(RelativeLayout.BELOW, imageId);
+		textView.setLayoutParams(relParams);
+		addView(textView);
+	}
+	
+	public TextView getTextView(){
+		return textView;
+	}
+	
+	public DishImageButton getDishImageButton(){
+		return imageButton;
 	}
 	
 	
-	
-	
+	public int convertDpToPx(Context context, int dp) {
+		float scale = context.getResources().getDisplayMetrics().density;
+		return (int) (dp * scale + 0.5f);
+	}
 }
